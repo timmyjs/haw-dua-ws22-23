f96261fa9eee63a837317451e9b1cabe8eabbc85
Thomas May <thomas.may@produktmacher.com>
2022-11-01 18:57:00 +0100

    Provide practice solutions from 25 Oct

--- main.js
+++
@@ -1,1 +0,0 @@
-document.getElementById("my-id").innerHTML = 'John Doe';
---
+++ src/main.js
@@ -0,0 +1,46 @@
+// Aufgabe 1
+const zahl = 42;
+
+console.log(zahl);
+
+const zeichenkette = "Thomas";
+
+console.log(zeichenkette);
+
+const flag = false;
+
+console.log(flag);
+
+const nixDa = undefined;
+
+console.log(nixDa);
+
+const nullDa = null;
+
+console.log(nullDa);
+
+let ichBinLeer;
+
+console.log(ichBinLeer);
+
+// console.log(baz);
+// Kommentar
+// Aufgabe 2
+{
+	const foo = 42;
+
+	const bar = "5";
+
+	console.log(foo + bar);
+
+	console.log(foo + parseInt(bar));
+}
+
+// Aufgabe 3
+{
+	const foo = "Wintersemester";
+
+	const bar = (22).toString() + (23).toString();
+
+	console.log(foo + bar);
+}
