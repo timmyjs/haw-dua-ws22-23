27f06fdf34e96637d0dae22f622ac49e3c230036
Thomas May <thomas.may@produktmacher.com>
2022-11-22 08:49:24 +0100

    Provide solutions for "Arrays in JavaScript II"

--- src/main.js
+++ src/main.js
@@ -1,46 +1,47 @@
-// Aufgabe 1
-const zahl = 42;
+/**
+ * WS 22/23 DuA - Übungsaufgaben: Arrays in JavaScript II
+ */
+console.log("%cÜbungsaufgabe: Arrays in JavaScript II", "color: #bf0b22");
+console.log("%c--", "color: #bf0b22");
 
-console.log(zahl);
+// 1.   Arrays sortieren
+// --
+// Sortieren Sie das Array 'numbers' so, dass die Zahlen in aufsteigender Reihenfolge vorkommen.
+// Regeln:
+// - Es dürfen nur diejenigen Elemente verwendet werden, welche bereits im Array angelegt sind
+// - Das Array darf zu keinem Zeitpunkt mehr als 5 oder weniger als 1 Eintrag haben
+// - Erlaubt sind die Array-Meethoden push(), pop(), shift(), unshift() sowie direkte Zuweisungen und Variablen
+const numbers = [5, 2, 4, 3, 1];
 
-const zeichenkette = "Thomas";
+/** Variante 1 */
+const five = numbers.shift();
+// [2, 4, 3, 1]
+const two = numbers.shift();
+// [4, 3, 1]
+const one = numbers.pop();
+// [4, 1]
+const three = numbers.pop();
+// [4]
+numbers.push(five);
+// [4, 5]
+numbers.unshift(three);
+// [3, 4, 5]
+numbers.unshift(two);
+// [2, 3, 4, 5]
+numbers.unshift(one);
+// [1, 2, 3, 4, 5]
+console.log(numbers);
 
-console.log(zeichenkette);
-
-const flag = false;
-
-console.log(flag);
-
-const nixDa = undefined;
-
-console.log(nixDa);
-
-const nullDa = null;
-
-console.log(nullDa);
-
-let ichBinLeer;
-
-console.log(ichBinLeer);
-
-// console.log(baz);
-// Kommentar
-// Aufgabe 2
-{
-	const foo = 42;
-
-	const bar = "5";
-
-	console.log(foo + bar);
-
-	console.log(foo + parseInt(bar));
-}
-
-// Aufgabe 3
-{
-	const foo = "Wintersemester";
-
-	const bar = (22).toString() + (23).toString();
-
-	console.log(foo + bar);
-}
+/** Variante 2
+ const five = numbers.shift();
+ // [2, 4, 3, 1]
+ const two = numbers.shift();
+ // [4, 3, 1]
+ const four = numbers.shift();
+ // [1]
+ const three = numbers.shift();
+ // [3, 1]
+ numbers.push(two, three, four, five);
+ // [1, 2, 3, 4, 5]
+ console.log(numbers);
+*/
